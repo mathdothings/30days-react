352fa83c8db22dfca405e31333e037fce9938645
mathdothings <jhonata.prodrigues@gmail.com>
2022-12-22 09:14:49 -0300

    done

--- day03-level-2-1/src/App.js
+++ day03-level-2-1/src/App.js
@@ -4,17 +4,17 @@ import css_logo from "./images/css-svgrepo-com.svg";
 import js_logo from "./images/js-svgrepo-com.svg";
 import react_logo from "./images/reactjs-svgrepo-com.svg";
 
-function svg(svgFile) {
-  return <img src={svgFile} alt="alt" />;
+function svg(svgFile, alt = "img alt") {
+  return <img src={svgFile} alt={alt} />;
 }
 
 function App() {
   return (
     <div className="App">
-      {svg(html_logo)}
-      {svg(css_logo)}
-      {svg(js_logo)}
-      {svg(react_logo)}
+      {svg(html_logo, "HTML Logo")}
+      {svg(css_logo, "CSS Logo")}
+      {svg(js_logo, "JS Logo")}
+      {svg(react_logo, "React Logo")}
     </div>
   );
 }
