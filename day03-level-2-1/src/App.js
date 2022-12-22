bf606f5af57554f0f40545bcc4407ec43247e41d
mathdothings <jhonata.prodrigues@gmail.com>
2022-12-22 09:00:30 -0300

    done

--- day03-level-2-1/src/App.js
+++ day03-level-2-1/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import html_logo from "./images/html-svgrepo-com.svg";
+import css_logo from "./images/css-svgrepo-com.svg";
+import js_logo from "./images/js-svgrepo-com.svg";
+import react_logo from "./images/reactjs-svgrepo-com.svg";
+
+function svg(svgFile) {
+  return <img src={svgFile} alt="alt" />;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {svg(html_logo)}
+      {svg(css_logo)}
+      {svg(js_logo)}
+      {svg(react_logo)}
     </div>
   );
 }
