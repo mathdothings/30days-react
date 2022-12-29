8d609bcb34871a1bb858a9307c06c76768387bfc
mathdothings <jhonata.prodrigues@gmail.com>
2022-12-29 11:03:14 -0300

    initial commit

--- day04/level2/exercises/src/App.jsx
+++ day04/level2/exercises/src/App.jsx
@@ -1,7 +1,43 @@
 import "./App.css";
+import html_logo from "./html-svgrepo-com.svg";
+import css_logo from "./css-svgrepo-com.svg";
+import js_logo from "./js-svgrepo-com.svg";
+import react_logo from "./reactjs-svgrepo-com.svg";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="logos">
+        <img src={html_logo} alt="" />
+        <img src={css_logo} alt="" />
+        <img src={js_logo} alt="" />
+        <img src={react_logo} alt="" />
+      </div>
+
+      <div>
+        <form className="form" action="">
+          <div>
+            <h1 className="title">Subscribe</h1>
+            <p className="message">
+              Sign up with your email address to receive news and updates.
+            </p>
+          </div>
+          <div className="form__input-container">
+            <input
+              className="text-input"
+              type="text"
+              placeholder="Frist name"
+            />
+            <input className="text-input" type="text" placeholder="Last name" />
+            <input className="text-input" type="text" placeholder="Email" />
+          </div>
+          <div>
+            <button className="submit-button">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
