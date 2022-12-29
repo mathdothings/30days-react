027c9294a6f740b586d617d9f5cbe497d1d33a2d
mathdothings <jhonata.prodrigues@gmail.com>
2022-12-29 11:42:18 -0300

    done

--- day04/level3/exercises/src/App.jsx
+++ day04/level3/exercises/src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css";
 
+// hexadecimal color generator
+const hexaColor = () => {
+  let str = "0123456789abcdef";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length);
+    color += str[index];
+  }
+  return color;
+};
+
+// random card's background color style generator
+const cardStyle = () => {
+  return { backgroundColor: `${hexaColor()}` };
+};
+
+// single card componet generator
+const Card = () => (
+  <div style={cardStyle()} className="card">
+    {hexaColor()}
+  </div>
+);
+
+// multiple card component generator
+const GenerateCards = (amout = 1) => {
+  const cards = [];
+  if (amout >= 1) {
+    for (let x = 0; x < amout; x++) {
+      cards.push(Card());
+    }
+  }
+
+  return cards;
+};
+
 function App() {
-  return <div></div>;
+  return <div>{GenerateCards(10)}</div>;
 }
 
 export default App;
