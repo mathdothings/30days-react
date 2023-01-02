35ab47c5f1dbd6d09574f9c5de82230a5347d17a
mathdothings <jhonata.prodrigues@gmail.com>
2023-01-02 08:44:41 -0300

    done

--- day04/level3/exercises/2/src/App.jsx
+++ day04/level3/exercises/2/src/App.jsx
@@ -1,7 +1,41 @@
 import "./App.css";
 
+const Header = () => (
+  <header>
+    <img src="https://via.placeholder.com/150" alt="" />
+  </header>
+);
+
+const Main = () => (
+  <main>
+    <h1>Mathdothings</h1>
+    <p>Junior Web Developer</p>
+    <h2>Skills</h2>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JS</li>
+      <li>React</li>
+    </ul>
+  </main>
+);
+
+const Footer = () => (
+  <footer>
+    <small>Coded by: @mathdothings</small>
+  </footer>
+);
+
+const Application = () => (
+  <article>
+    <Header />
+    <Main />
+    <Footer />
+  </article>
+);
+
 function App() {
-  return <div></div>;
+  return <Application />;
 }
 
 export default App;
