51ee7bd8f173755a917b9509c6987185b09f488d
mathdothings <jhonata.prodrigues@gmail.com>
2023-01-03 11:56:48 -0300

    wip

--- day05/core/lesson/src/App.jsx
+++ day05/core/lesson/src/App.jsx
@@ -1,29 +1,38 @@
 import "./App.css";
 
-//component syntax
-// User component, component should start with an uppercase
-const User = (props) => (
-  <div>
-    <h1 className={props.class}>
-      {props.firstName}
-      {props.lastName}
-    </h1>
-    <small>{props.country}</small>
-  </div>
-);
+// Fuction to show month date year
+const showDate = (time) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-const welcome = "Welcome to 30 Days Of React";
-const title = "Getting Started React";
-const subtitle = "JavaScript Library";
-const author = {
-  firstName: "Asabeneh",
-  lastName: "Yetayeh",
+  const month = months[time.getMonth()].slice(0, 3);
+  const year = time.getFullYear();
+  const date = time.getDate();
+  return ` ${month} ${date}, ${year}`;
 };
-const date = "Oct 4, 2020";
 
 // Header Component
-const Header = (props) => {
-  // console.log(props); // empty object, {}
+const Header = ({
+  data: {
+    welcome,
+    title,
+    subtitle,
+    author: { firstName, lastName },
+    date,
+  },
+}) => {
   return (
     <header>
       <div className="header-wrapper">
@@ -31,23 +40,111 @@ const Header = (props) => {
         <h2>{title}</h2>
         <h3>{subtitle}</h3>
         <p>
-          {author.firstName} {author.lastName}
+          {firstName} {lastName}
         </p>
-        <small>{date}</small>
+        <small>{showDate(date)}</small>
       </div>
     </header>
   );
 };
 
-const Input = (attributes) => <input {...attributes} />;
+// TechList Component
+const TechList = ({ techs }) => {
+  const techList = techs.map((tech) => <li key={tech}>{tech}</li>);
+  return techList;
+};
 
-function App() {
+// User Card Component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className="user-card">
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
+);
+
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+);
+
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: "#61dbfb",
+  padding: 10,
+  border: "none",
+  borderRadius: 5,
+  margin: 3,
+  cursor: "pointer",
+  fontSize: 18,
+  color: "white",
+};
+
+// Main Component
+const Main = ({ user, techs, greetPeople, handleTime }) => (
+  <main>
+    <div className="main-wrapper">
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList techs={techs} />
+      </ul>
+      <UserCard user={user} />
+      <Button text="Greet People" onClick={greetPeople} style={buttonStyles} />
+      <Button text="Show Time" onClick={handleTime} style={buttonStyles} />
+    </div>
+  </main>
+);
+
+// Footer Component
+const Footer = ({ copyRight }) => (
+  <footer>
+    <div className="footer-wrapper">
+      <p>Copyright {copyRight.getFullYear()}</p>
+    </div>
+  </footer>
+);
+
+// The App, or the parent or the container component
+// Functional Component
+const App = () => {
+  const data = {
+    welcome: "Welcome to 30 Days Of React",
+    title: "Getting Started React",
+    subtitle: "JavaScript Library",
+    author: {
+      firstName: "Asabeneh",
+      lastName: "Yetayeh",
+    },
+    date: new Date(), // date needs to be formatted to a human readable format
+  };
+  const date = new Date();
+  const techs = ["HTML", "CSS", "JavaScript"];
+  // copying the author from data object to user variable using spread operator
+  const user = { ...data.author }; //image: asabenehImage }
+
+  const handleTime = () => {
+    alert(showDate(new Date()));
+  };
+  const greetPeople = () => {
+    alert("Welcome to 30 Days Of React Challenge, 2020");
+  };
+
   return (
-    <div>
-      <Header />
-      <Input type="number" />
+    <div className="app">
+      <Header data={data} />
+      <Main
+        user={user}
+        techs={techs}
+        handleTime={handleTime}
+        greetPeople={greetPeople}
+      />
+      <Footer copyRight={date} />
     </div>
   );
-}
+};
 
 export default App;
