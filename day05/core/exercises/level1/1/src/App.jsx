218fa0e5a303611fcb621faab7a9c1e54d62977f
mathdothings <jhonata.prodrigues@gmail.com>
2023-01-03 20:56:05 -0300

    done

--- day05/core/exercises/level1/1/src/App.jsx
+++ day05/core/exercises/level1/1/src/App.jsx
@@ -1,6 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+/* 
+  1. What is props in a React component?
+      Props is a parameter that can be passed as a react component. This way we can write reusable components, passing data through then as props.
+  2. How do you access props in a React component?
+      Props can be acessed in many ways. We can acess it through dynamicall variables, object literal, destructuring and much more.
+  3. What data types can we pass as props to components?
+      We can pass any primitive data type as well as arrays, functions and objects.
+  4. What is a propTypes?
+      PropType is a package that helps us to define and handle datatypes in React components.
+  5. What is a default propTypes?
+      Default propTypes is a tool to set some default values to a component proprieties datatypes.
+ */
 function App() {
   return (
     <div className="App">
