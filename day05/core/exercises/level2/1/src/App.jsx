1b701f00b834a6f18996b59f6bcc4c94bee54f7a
mathdothings <jhonata.prodrigues@gmail.com>
2023-01-04 14:09:48 -0300

    wip

--- day05/core/exercises/level2/1/src/App.jsx
+++ day05/core/exercises/level2/1/src/App.jsx
@@ -1,17 +1,62 @@
-import logo from "./logo.svg";
 import "./App.css";
 import htmlLogo from "./html-svgrepo-com.svg";
 import cssLogo from "./css-svgrepo-com.svg";
 import jsLogo from "./js-svgrepo-com.svg";
 import reactLogo from "./reactjs-svgrepo-com.svg";
 
+const updateProfile = ({ name, age, nacionality, location }) => {
+  return `${name} ${age} ${nacionality} ${location}`;
+};
+
+const Elon = {
+  name: "Elon Musk",
+  age: 41,
+  nacionality: "USA",
+  location: "Space X",
+};
+
+console.log(updateProfile(Elon));
+
+const updateList = ([first, second, third]) => {
+  return `${first} ${second} ${third}`;
+};
+
+console.log(updateList([10, 3, 7]));
+
 const Image = (props) => {
   return <img src={props.imgSrc} alt={props.imgAlt} />;
+};
+
+const Title = ({ content, color, fontFamily, textAlign }) => {
+  return (
+    <h1 style={{ color: color, fontFamily: fontFamily, textAlign: textAlign }}>
+      {content}
+    </h1>
+  );
+};
+
+const Button = () => {
+  return (
+    <button
+      onClick={() => {
+        return <Title content={"Nani?!"} />;
+      }}
+    >
+      Nani?!
+    </button>
+  );
 };
 
 function App() {
   return (
     <div>
+      <Title
+        content={"Hello, world!"}
+        color={"yellow"}
+        fontFamily={"monospace"}
+        textAlign={"center"}
+      />
+      <Button />
       <Image imgSrc={htmlLogo} imgAlt="html logo" />
       <Image imgSrc={cssLogo} imgAlt="css logo" />
       <Image imgSrc={jsLogo} imgAlt="js logo" />
