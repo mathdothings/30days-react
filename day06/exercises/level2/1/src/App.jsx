f96679082d603cf76432baa8e030d6beef4669f8
mathdothings <jhonata.prodrigues@gmail.com>
2023-01-10 07:02:44 -0300

    fix: move helpers functions to a appropriate file

--- day06/exercises/level2/1/src/App.jsx
+++ day06/exercises/level2/1/src/App.jsx
@@ -1,6 +1,7 @@
-import { useInsertionEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import generateNumbers from "./helpers";
+
+const numbers = generateNumbers();
 
 function Numbers({ numbers }) {
   return numbers.map(({ backgroundColor, value }, index) => (
@@ -16,67 +17,6 @@ function App() {
       <Numbers numbers={generateNumbers(0, 32)} />
     </ul>
   );
-}
-
-function isPrime(number) {
-  let divisors = 0;
-
-  if (number === 1) {
-    return true;
-  }
-
-  if (number > 2 && number % 2 === 0) {
-    return false;
-  }
-
-  for (let x = 0; x <= number; x += 1) {
-    if (number % x === 0) {
-      divisors += 1;
-    }
-  }
-
-  if (divisors === 2) {
-    return true;
-  }
-
-  return false;
-}
-
-function isEven(number) {
-  return number % 2 === 0;
-}
-
-function isOdd(number) {
-  return !isEven(number);
-}
-
-function generateNumbers(start, end) {
-  const numbers = [];
-  for (let x = start; x < end; x += 1) {
-    numbers.push({
-      value: x,
-      backgroundColor: determineBackgroundColor(x),
-    });
-  }
-
-  return numbers;
-}
-
-function determineBackgroundColor(number) {
-  let style = "";
-  if (isEven(number)) {
-    style = "is-even";
-  }
-
-  if (isOdd(number)) {
-    style = "is-odd";
-  }
-
-  if (isPrime(number)) {
-    style = "is-prime";
-  }
-
-  return style;
 }
 
 export default App;
---
+++ day06/exercises/level2/1/src/helpers.js
@@ -0,0 +1,62 @@
+function isPrime(number) {
+  let divisors = 0;
+
+  if (number === 1) {
+    return true;
+  }
+
+  if (number > 2 && number % 2 === 0) {
+    return false;
+  }
+
+  for (let x = 0; x <= number; x += 1) {
+    if (number % x === 0) {
+      divisors += 1;
+    }
+  }
+
+  if (divisors === 2) {
+    return true;
+  }
+
+  return false;
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function isOdd(number) {
+  return !isEven(number);
+}
+
+function generateNumbers(start, end) {
+  const numbers = [];
+  for (let x = start; x < end; x += 1) {
+    numbers.push({
+      value: x,
+      backgroundColor: determineBackgroundColor(x),
+    });
+  }
+
+  return numbers;
+}
+
+function determineBackgroundColor(number) {
+  let style = "";
+  if (isEven(number)) {
+    style = "is-even";
+  }
+
+  if (isOdd(number)) {
+    style = "is-odd";
+  }
+
+  if (isPrime(number)) {
+    style = "is-prime";
+  }
+
+  return style;
+}
+
+export default generateNumbers;
