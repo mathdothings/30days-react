3da42397a1353ad2bcaad8e6a283352e4c4dabe6
mathdothings <jhonata.prodrigues@gmail.com>
2023-01-14 21:10:48 -0300

    done

--- day06/exercises/level2/3/src/App.jsx
+++ day06/exercises/level2/3/src/App.jsx
@@ -1,76 +1,70 @@
 import "./App.css";
 
 const data = [
-  { place: "world", value: 7693165599 },
+  { place: "World", value: 7693165599 },
   {
-    place: "china",
+    place: "China",
     value: 1377422166,
   },
   {
-    place: "india",
+    place: "India",
     value: 1295210000,
   },
   {
-    place: "usa",
+    place: "USA",
     value: 323947000,
   },
   {
-    place: "indonesia",
+    place: "Indonesia",
     value: 258705000,
   },
   {
-    place: "brazil",
+    place: "Brazil",
     value: 206135893,
   },
   {
-    place: "pakistan",
+    place: "Pakistan",
     value: 194125062,
   },
   {
-    place: "nigeria",
+    place: "Nigeria",
     value: 186988000,
   },
   {
-    place: "bangladesh",
+    place: "Bangladesh",
     value: 161006790,
   },
   {
-    place: "russia",
+    place: "Russia",
     value: 146599183,
   },
   {
-    place: "japan",
+    place: "Japan",
     value: 126960000,
   },
 ];
 
-const Box = () => {
+const calculatePercentages = () => {
+  const totalPopulation = data[0].value;
   const range = data[1].value - data[data.length - 1].value;
-  const step = Math.floor(range / 100);
-  const total = step * 100;
+  const step = Math.floor(totalPopulation / 100); // step is equivalent to 1% of the total population
+  return data.map((item) => `${item.place} ${(item.value / step).toFixed(2)}`);
+};
+
+const Box = () => {
+  const percentages = calculatePercentages();
   return (
     <div className="box">
-      <Bar percentage={"10%"} amount={"10%"} />
-      <Bar percentage={"20%"} amount={"20%"} />
-      <Bar percentage={"30%"} amount={"30%"} />
-      <Bar percentage={"40%"} amount={"40%"} />
-      <Bar percentage={"50%"} amount={"50%"} />
-      <Bar percentage={"60%"} amount={"60%"} />
-      <Bar percentage={"70%"} amount={"70%"} />
-      <Bar percentage={"80%"} amount={"80%"} />
-      <Bar percentage={"90%"} amount={"90%"} />
-      <Bar percentage={"100%"} amount={"100%"} />
-      {/* {console.log("range:", range)}
-      {console.log("step:", step)}
-      {console.log("total:", total)} */}
-      <p>I should do this boring math?</p>
+      {percentages.map((item, index) => (
+        <Bar key={index} percentage={item.split(" ")[1]} amount={item + "%"} />
+      ))}
     </div>
   );
 };
 
 const Bar = ({ percentage, amount }) => (
-  <div className="bar" style={{ minWidth: percentage, maxWidth: "500px" }}>
-    {amount}
+  <div className="bar" style={{ width: `${percentage}%`, maxWidth: "500px" }}>
+    <pre style={{ margin: 0 }}>{amount}</pre>
   </div>
 );
 
