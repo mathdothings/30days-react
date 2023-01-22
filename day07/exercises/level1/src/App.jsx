3be9d76a0038368cf07ad4d6ef4fdcc05bcdf9f3
mathdothings <jhonata.prodrigues@gmail.com>
2023-01-22 19:57:22 -0300

    done

--- day07/exercises/level1/src/App.jsx
+++ day07/exercises/level1/src/App.jsx
@@ -3,15 +3,24 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 /* 
-1. How do you write a pure JavaScript function
+1. How do you write a pure JavaScript function?
+    You could use regular function or arrow function.
+    a. Regular Function: function main () {// function code};
+    b. Arrow Function: const main = () => {// function code};
 2. What is inheritance and how do you make a child from a parent class?
-3. What is class based React component ?
-4. What is the difference between functional React component and class based React component ?
-5. When do we need to use class based components instead of functional components
-6. What is the use cases of class based component ?
-7. Which type of component do use most frequently ? functional or class-based component
-8. What is React life cycle ? (not covered yet) ?
-9. What is state in React ? (not covered yet)
+    Inheritance occurs when we create a new object from a parent class. The new object will have some properties and method inherited from the parent class.
+3. What is class based React component?
+    It is just a react component but writted with class syntax.
+4. What is the difference between functional React component and class based React component?
+    Class components has its own way to deal with state and lifecycle. Function components needs the usage of hooks to work with state and lifecycle.
+5. When do we need to use class based components instead of functional components?
+    I can't find a scenario where use class based components is better than functional one.
+6. What is the use cases of class based component?
+    Same use cases as functional components.
+7. Which type of component do use most frequently? functional or class-based component
+    Functional.
+8. What is React life cycle? (not covered yet)
+9. What is state in React? (not covered yet)
 */
 
 function App() {
