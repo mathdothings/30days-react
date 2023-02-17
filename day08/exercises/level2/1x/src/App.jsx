ef47addc6de1a2bd3ebd9b82f2579d6a089b8c70
mathdothings <jhonata.prodrigues@gmail.com>
2023-02-17 08:39:19 -0300

    wip

--- day08/exercises/level2/1x/src/App.jsx
+++ day08/exercises/level2/1x/src/App.jsx
@@ -3,24 +3,19 @@ import React from "react";
 import { Sun, Moon } from "react-feather";
 
 class Navegation extends React.Component {
-  capitalize = (word) => {
-    const firstLetter = word.charAt(0).toLocaleUpperCase();
-    return `${firstLetter + word.slice(1)}`;
-  };
-
   render() {
     return (
       <nav className="nav">
-        <ul className={`${this.props.theme} nav_menu`}>
+        <ul className="nav_menu">
           <li className="nav_menu_item">Home</li>
           <li className="nav_menu_item">Products</li>
           <li className="nav_menu_item">Testimonials</li>
           <li className="nav_menu_item">About</li>
           <li
-            className="nav_menu_item nav_menu_item--theme-button"
+            className={`${this.props.theme} nav_menu_item nav_menu_item--theme-button`}
             onMouseEnter={this.props.changeState}
           >
-            {this.props.themes}
+            {this.props.changeIcon()}
           </li>
         </ul>
       </nav>
@@ -29,14 +24,37 @@ class Navegation extends React.Component {
 }
 
 class App extends React.Component {
-  state = { theme: <Sun /> };
-  changeState = (key, value) => {
-    this.setState({ [key]: value });
+  state = { theme: "light" };
+
+  changeState = () => {
+    const theme = this.state.theme === "light" ? "dark" : "light";
+
+    if (theme === "light") {
+      this.setState({ theme });
+    }
+    if (theme === "dark") {
+      this.setState({ theme });
+    }
   };
+
+  changeIcon = () => {
+    if (this.state.theme === "light") return <Sun />;
+    if (this.state.theme === "dark") return <Moon />;
+  };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <>
-        <Navegation theme={this.state.theme} changeState={this.changeState} />
+        <Navegation
+          theme={this.state.theme}
+          themeIcon={this.state.themeIcon}
+          changeState={this.changeState}
+          changeIcon={this.changeIcon}
+        />
       </>
     );
   }
