55158de843713ec443578e7966213550499821a4
mathdothings <jhonata.prodrigues@gmail.com>
2023-02-19 16:31:40 -0300

    wip

--- day08/exercises/level2/c1/src/App.jsx
+++ day08/exercises/level2/c1/src/App.jsx
@@ -3,8 +3,18 @@ import "./App.css";
 import Projects from "./Components";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        { title: "Business Website", category: "Web Design" },
+        { title: "Social App", category: "Mobile Development" },
+        { title: "Ecommerce Shopping Cart", category: "Web Development" },
+      ],
+    };
+  }
   render() {
-    return <Projects />;
+    return <Projects projects={this.state.projects} />;
   }
 }
 
--- day08/exercises/level2/c1/src/Components.jsx
+++ day08/exercises/level2/c1/src/Components.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
+class ProjectItem extends React.Component {
+  render() {
+    const project = this.props.project;
+    const { title, category } = project;
+    return (
+      <li>
+        {title} - {category}
+      </li>
+    );
+  }
+}
+
 class Projects extends React.Component {
   render() {
+    const projects = this.props.projects;
     return (
-      <>
-        <p>My Projects</p>
-      </>
+      <ul>
+        {projects.map((item, index) => (
+          <ProjectItem project={item} key={index} />
+        ))}
+      </ul>
     );
   }
 }
